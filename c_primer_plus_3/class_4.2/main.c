eb2be38cc2f60c72dcf46889338a72b46ba16e5d
xujiagen <xjiagen@gmail.com>
2018-02-09 19:11:01 +0800

    c语言方式创建结构体

--- c_primer_plus_3/class_4.2/main.c
+++ c_primer_plus_3/class_4.2/main.c
@@ -3,11 +3,13 @@ struct Person {
 	char name[30];
 	char address[100];
 };
-int main()
+int main()//c语言里面结构体没有公有的和私有的概念
 {
 	struct Person p;
 	strcpy(p.name, "Bill");
 	strcpy(p.address, "花园街5号");
+
+	printf("%s,%s", p.name, p.address);
 	printf("hello C!\n");
 	return 0;
 }
